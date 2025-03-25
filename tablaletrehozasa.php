a9ef4d7b0cda5f0cf2fa3117bfbba2676b8908aa
DirvaVoped <72589360134@szily.hu>
2025-03-25 08:36:57 +0100

    tablaletrehozas elott

--- tablaletrehozasa.php
+++ tablaletrehozasa.php
@@ -8,3 +8,36 @@ if($conn->connect_error)
 {
     die("Csatlakozasi hiba". $conn->connect_error);
 }
+else
+{
+    echo "Csatlakozas megtortent";
+}
+
+$sql= "Drop database if not exists emberek";
+if($conn->query($sql) === true)
+{
+    echo"Sikeres tablarorles";
+}
+else
+{
+    echo "Nem sikerult a tablatorles";
+}
+$sql="";
+$sql="CREATE TABLE if not EXISTS emberek(
+    id INT unsigned AUTO_INCREMENT PRIMARY KEY,
+    vezeteknev VARCHAR(50) NOT NULL,
+    keresztnev VARCHAR(50) not null,
+    beosztas VARCHAR(50) not null,
+    regiszt_datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	)";
+if($conn->query($sql) === true)
+{
+    echo"Sikeres tablarorles";
+}
+else
+{
+    echo "Nem sikerult a tablatorles";
+}
+
+
+$conn->close();
